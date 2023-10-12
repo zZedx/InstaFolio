bbda66fa0886c068a6e2fbc0f9a69537e669e115
zZedx <Kartikajmera890@gmail.com>
2023-10-12 18:54:41 +0530

    more style on project

--- src/pages/home/Project.jsx
+++ src/pages/home/Project.jsx
@@ -1,5 +1,7 @@
+import TextExpander from "../../utils/textExpander";
+
 const Project = ({ project }) => {
-  const { name, date, img, tools , description} = project;
+  const { name, date, img, tools, description } = project;
   return (
     <div className="border border-white/20">
       <nav className="flex gap-3 px-4 py-3 items-center">
@@ -7,21 +9,34 @@ const Project = ({ project }) => {
         <span className="w-1 h-1 rounded-full bg-gray-400"></span>
         <span className="text-sm text-gray-400">{date}</span>
       </nav>
-      <img className=" h-56" src={img} alt="" />
+      <img className="w-full h-56" src={img} alt="" />
       <main>
         <ul className="flex my-4 px-4 gap-3">
           {tools.map((tool, i) => (
-            <li key={i}><Tool>{tool}</Tool></li>
+            <li key={i}>
+              <Tool>{tool}</Tool>
+            </li>
           ))}
         </ul>
-        <p className="px-4 mb-3">{description}</p>
+        <TextExpander
+          colapsedNumWords={5}
+          buttonInline={false}
+          buttonColor="pink"
+          className="px-4 mb-3"
+        >
+          {description}
+        </TextExpander>
       </main>
     </div>
   );
 };
 
 function Tool({ children }) {
-  return <span className="bg-white/40 px-2 py-0.5 inline-block rounded-lg uppercase font-semibold text-sm">{children}</span>;
+  return (
+    <span className="bg-white/40 px-2 py-0.5 inline-block rounded-lg uppercase font-semibold text-sm">
+      {children}
+    </span>
+  );
 }
 
 export default Project;
