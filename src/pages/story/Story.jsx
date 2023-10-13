1b4f5bbf7072b2143931b6e955b3259836af2ae9
zZedx <Kartikajmera890@gmail.com>
2023-10-13 21:56:13 +0530

    story design 2

--- src/pages/story/Story.jsx
+++ src/pages/story/Story.jsx
@@ -12,7 +12,7 @@ import { recent } from "../../utils/recentProjecs";
 
 const MAX_VISIBILITY = 3;
 
-const Card = ({ title, content, imgs, tools }) => {
+const Card = ({ title, content, imgs, tools , link }) => {
   const { id } = useParams();
   useEffect(() => {
     setActive(false);
@@ -20,15 +20,72 @@ const Card = ({ title, content, imgs, tools }) => {
   const [active, setActive] = useState(false);
   return (
     <div
-      className={`card flex flex-col justify-between items-center overflow-hidden ${
-        active ? "cursor-pointer" : ""
+      className={`card overflow-hidden bg-cover bg-center flex items-end border-2 ${
+        active ? "cursor-pointer backdrop-blur-lg" : ""
       }`}
       style={{
-        boxShadow: active ? "0 0 40px purple, 0 0 150px purple" : "none",
+        boxShadow: active ? "0 0 40px hsl(327, 87%, 14%), 0 0 150px hsl(327, 87%, 14%)" : "none",
+        backgroundImage: `url(${imgs[0]})`,
       }}
       onClick={() => setActive(false)}
     >
-      <span
+      <div
+        className={`p-6 w-full transition-all duration-500 ease-in-out space-y-4 ${
+          active ? "translate-y-0" : "2xl:translate-y-[76%] translate-y-[75%]"
+        }`}
+        style={{
+          background:
+            "linear-gradient(180deg, hsl(327, 32%, 1% , 1), hsl(327, 87%, 14%))",
+        }}
+      >
+        <div className="flex items-center justify-between mb-6 ">
+          <h1
+            className={`z-10 font-bold text-xl 2xl:text-2xl relative tracking-wider text-transparent uppercase after:h-1 after:w-[120%] after:bg-white/70 after:absolute after:bottom-[-5px] after:left-0 after:origin-left after:transition-all after:duration-1000 ${
+              active ? "after:scale-100" : "after:scale-0"
+            }`}
+            style={{
+              // WebkitTextStroke: "2px white",
+              textShadow:
+                "0 0 40px rgb(33, 235, 255), 0 0 0px rgb(33, 235, 255) , 0 0 150px rgb(33, 235, 255)",
+            }}
+          >
+            {title}
+          </h1>
+          {active ? (
+            <a href={link}>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setActive(true);
+                }}
+                className={`custom-btn btn2 text-sm py-2 px-4 rounded-sm flex gap-3 items-center transition-all`}
+              >
+                <i className="fa-brands fa-github text-lg"></i> <span>CODE</span>
+              </button>
+            </a>
+          ) : (
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                setActive(true);
+              }}
+              className={`custom-btn btn text-sm py-2 px-4 rounded-sm flex gap-3 items-center transition-all`}
+            >
+              <span>DETAILS</span> <i className="fa-solid fa-arrow-down"></i>
+            </button>
+          )}
+        </div>
+
+        <ul className="flex gap-3 ">
+          {tools.map((tool, i) => (
+            <li key={i}>
+              <Tool>{tool}</Tool>
+            </li>
+          ))}
+        </ul>
+        <p className="border-t-2 pt-3 h-48">{content}</p>
+      </div>
+      {/* <span
         className="z-10 font-bold text-6xl tracking-wider text-transparent uppercase"
         style={{
           WebkitTextStroke: "2px white",
@@ -38,7 +95,7 @@ const Card = ({ title, content, imgs, tools }) => {
         {title}
       </span>
       <img
-        className={` transition-all absolute top-0 left-0 h-full object-cover object-center z-0 p-1 rounded-2xl ${
+        className={` transition-all absolute top-0 left-0 h-full object-cover object-center z-0 ${
           active ? "blur-lg" : ""
         }`}
         src={imgs[0]}
@@ -56,15 +113,19 @@ const Card = ({ title, content, imgs, tools }) => {
         </ul>
 
         {!active && (
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              setActive(true);
-            }}
-            className="  px-3 py-2 text-purple-400 cursor-pointer text-xl underline hover:text-purple-200 w-full uppercase font-semibold"
-          >
-            more Details
-          </button>
+          // <button
+          //   onClick={(e) => {
+          //     e.stopPropagation();
+          //     setActive(true);
+          //   }}
+          //   className="  px-3 py-2 text-purple-400 cursor-pointer text-xl underline hover:text-purple-200 w-full uppercase font-semibold"
+          // >
+          //   more Details
+          // </button>
+          <button onClick={(e) => {
+            e.stopPropagation();
+            setActive(true);
+          }} className="custom-btn btn-11">MORE DETAILS</button>
         )}
         <div
           className={`transition-all duration-500 ease-out translate-y-96 absolute w-3/4 text-center space-y-3 ${
@@ -79,14 +140,14 @@ const Card = ({ title, content, imgs, tools }) => {
             CODE
           </a>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
 
 function Tool({ children }) {
   return (
-    <span className="bg-gradient-to-tr to-purple-400 border-2 border-red-700 from-pink-500 px-2 py-0.5 inline-block rounded uppercase font-semibold text-xs md:text-sm min-w-[80px] text-center">
+    <span className="bg-gradient-to-tr to-[#E4F9F5] border-2 border-purple-800 from-[#11999E] px-2 py-0.5 inline-block rounded uppercase font-bold text-xs md:text-sm min-w-[80px] text-center text-black">
       {children}
     </span>
   );
@@ -102,10 +163,12 @@ const Carousel = () => {
     onSwipedLeft: () => {
       if (active === count - 1) return;
       setActive(active + 1);
+      navigate(`/story/${Number(id) + 1}`);
     },
     onSwipedRight: () => {
       if (active === 0) return;
       setActive(active - 1);
+      navigate(`/story/${Number(id) - 1}`);
     },
   });
 
@@ -153,6 +216,7 @@ const Carousel = () => {
               title={project.name}
               content={project.description}
               tools={project.tools}
+              link = {project.link}
             />
           </div>
         ))}
--- src/utils/recentProjecs.js
+++ src/utils/recentProjecs.js
@@ -4,48 +4,48 @@ export const recent = [
       imgs: ["../images/yelpCamp.png"],
       description: "sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada sadawdawdada",
       tools: ["html" , "css" , "js"],
-      date: "",
+      link : ''
     },
     {
       name: "Spotify",
       imgs: ["../images/spotify.png"],
       description: "sadawdawdada",
       tools: [ "css" , "js"],
-      date: "",
+      link : ''
     },
     {
       name: "Spotify",
       imgs: ["../images/spotify.png" ,"../images/spotify.png" ,"../images/spotify.png"],
       description: "sadawdawdada",
       tools: [ "css" , "js"],
-      date: "",
+      link : ''
     },
     {
       name: "Spotify",
       imgs: ["../images/spotify.png"],
       description: "sadawdawdada sadawdawdada",
       tools: [ "css" , "js"],
-      date: "",
+      link : ''
     },
     {
       name: "Spotify",
       imgs: ["../images/spotify.png"],
       description: "",
       tools: [ "css" , "js"],
-      date: "",
+      link : ''
     },
     {
       name: "Spotify",
       imgs: ["../images/spotify.png"],
       description: "",
       tools: [ "css" , "js"],
-      date: "",
+      link : ''
     },
     {
       name: "Spotify",
       imgs: ["../images/spotify.png"],
       description: "",
       tools: [ "css" , "js"],
-      date: "",
+      link : ''
     }
   ];
