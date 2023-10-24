89cf4109b63987916eda57e3751ed65fb3758e6a
zZedx <Kartikajmera890@gmail.com>
2023-10-24 13:37:28 +0530

    typo fix

--- src/pages/home/FvrtProjects.jsx
+++ src/pages/home/FvrtProjects.jsx
@@ -6,7 +6,7 @@ const projects = [
     img: "https://res.cloudinary.com/dbm00ix5k/image/upload/v1697533367/InstaFolio/Domihut_arfo1n.png",
     description:
       "I created 'DomiHut Pizza,' a web-based pizza ordering system developed with React, React Router, Redux Toolkit, and Tailwind CSS. This project demonstrates my proficiency in using cutting-edge web development technologies to build a dynamic and user-friendly application. Users can easily navigate the menu, add items to their cart, and place orders. The use of React and React Router ensures a responsive and seamless user experience, while Redux Toolkit manages the state efficiently. 'DomiHut Pizza' boasts a modern and visually appealing design, thanks to Tailwind CSS. This project is a testament to my skills as a web developer and a valuable addition to my resume and portfolio, showcasing my ability to create practical and engaging web applications.",
-    tools: ["React", "Redux", "TailwindCss"],
+    tools: ["React", "Redux", "Tailwind Css"],
     date: "9 Oct 2023",
     code: "https://github.com/zZedx/DomiHut",
     live: "https://domihut-jgpvuyyqd-zzedx.vercel.app",
@@ -17,7 +17,7 @@ const projects = [
     img: "https://res.cloudinary.com/dbm00ix5k/image/upload/v1697533366/InstaFolio/chatter_nxi1wp.png",
     description:
       "Chatter is a dynamic real-time chat application that I developed using a modern technology stack, including HTML, CSS, JavaScript, MongoDB, Express.js, Node.js, and Socket.io. This project showcases my proficiency in full-stack web development and my ability to create interactive and engaging user experiences. It features real-time messaging, allowing users to engage in instant, seamless conversations. User authentication is implemented to ensure secure access, enabling users to create accounts, log in, and protect their privacy. Additionally, Chatter stores message history in a MongoDB database, ensuring that users can access their conversations even after logging out. The application's responsive design adapts to various screen sizes, making it accessible on both desktop and mobile devices.",
-    tools: ["JavaScipt", "Node", "Socket.io"],
+    tools: ["JavaScript", "Node", "Socket.io"],
     date: "1 month ago",
     code: "https://github.com/zZedx/Chatter",
     live: "https://chatter1.onrender.com",
@@ -37,7 +37,7 @@ const projects = [
     img: "https://res.cloudinary.com/dbm00ix5k/image/upload/v1697533367/InstaFolio/instafolio_bn5tem.png",
     description:
       "Developed an Instagram-themed portfolio website using React, React Router, and Tailwind CSS, which serves as a compelling showcase of my web development skills. This project underscores my ability to create dynamic and responsive interfaces with React, ensuring an engaging user experience. The implementation of React Router enables seamless navigation between different sections, enhancing the user's interaction with my portfolio. Tailwind CSS was utilized to craft a modern and visually appealing design, mirroring the aesthetics of Instagram itself. This project not only demonstrates my proficiency but also stands as a noteworthy addition to my resume and portfolio, showcasing my capabilities as a web developer.",
-    tools: ["React", "react router", "Tailwind"],
+    tools: ["React", "react router", "Tailwind Css"],
     date: "1 Week ago",
     code: "https://github.com/zZedx/InstaFolio",
     live: "https://insta-folio-87si03k13-zzedx.vercel.app/",
